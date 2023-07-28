88af44d055c958cd6fcddbcf5e9424ff2bc7f681
jose-pedro <josepedro.bernardes1@gmail.com>
2023-07-28 11:59:53 -0300

    feat: add new entity: users

---
+++ src/entities/users.entity.ts
@@ -0,0 +1,49 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+  BeforeInsert,
+  BeforeUpdate,
+} from "typeorm";
+
+import { getRounds, hashSync } from "bcryptjs";
+
+import Contact from "./contacts.entity";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column({ type: "varchar", length: 45 })
+  full_name: string;
+
+  @Column({ type: "varchar", length: 125, unique: true })
+  email: string;
+
+  @Column({ type: "varchar", length: 11, unique: true })
+  phone_number: string;
+
+  @CreateDateColumn({ type: "date" })
+  createdAt: Date;
+
+  @Column({ type: "varchar", length: 120 })
+  password: string;
+
+  @OneToMany(() => Contact, (contacts) => contacts.user, { nullable: true })
+  contacts: Contact[];
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    const isEncrypted = getRounds(this.password);
+
+    if (!isEncrypted) {
+      this.password = hashSync(this.password, 10);
+    }
+  }
+}
+
+export default User;
