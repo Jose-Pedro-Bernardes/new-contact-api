59e628308ceeeb5a559cc00d0516a465044f552e
jose-pedro <josepedro.bernardes1@gmail.com>
2023-07-28 12:00:16 -0300

    feat: add new entity: contacts

---
+++ src/entities/contacts.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  ManyToOne,
+} from "typeorm";
+import User from "./users.entity";
+
+@Entity("contacts")
+class Contact {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column({ type: "varchar", length: 45 })
+  full_name: string;
+
+  @Column({ type: "varchar", length: 125, unique: true })
+  email: string;
+
+  @Column({ type: "varchar", length: 11, unique: true })
+  phone_number: string;
+
+  @CreateDateColumn({ type: "date" })
+  createdAt: Date;
+
+  @ManyToOne(() => User)
+  user: User;
+}
+
+export default Contact;
